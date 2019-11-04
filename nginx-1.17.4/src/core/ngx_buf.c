45208a258da7fba20d6391831268fde087797cde
176596609 <176596609@qq.com>
2019-11-04 21:30:24 +0800

    chain update

--- nginx-1.17.4/src/core/ngx_buf.c
+++ nginx-1.17.4/src/core/ngx_buf.c
@@ -8,17 +8,22 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/**
+ * 创建一个缓冲区。需要传入pool和buf的大小
+ */
 ngx_buf_t *
 ngx_create_temp_buf(ngx_pool_t *pool, size_t size)
 {
     ngx_buf_t *b;
 
+    /*
+       ngx_calloc_buf 展开是 ngx_pcalloc(pool, sizeof(ngx_buf_t))
+    */
     b = ngx_calloc_buf(pool);
     if (b == NULL) {
         return NULL;
     }
-
+    /* 分配缓冲区内存;  pool为内存池，size为buf的大小*/
     b->start = ngx_palloc(pool, size);
     if (b->start == NULL) {
         return NULL;
@@ -35,27 +40,30 @@ ngx_create_temp_buf(ngx_pool_t *pool, size_t size)
      *     and flags
      */
 
-    b->pos = b->start;
-    b->last = b->start;
-    b->end = b->last + size;
+    b->pos = b->start; //待处理数据的标记指针
+    b->last = b->start;//待处理数据的结尾标记指针
+    b->end = b->last + size;//缓冲区结尾地址
     b->temporary = 1;
 
     return b;
 }
 
-
+/*创建一个缓冲区的链表结构 ngx_alloc_chain_link*/
 ngx_chain_t *
 ngx_alloc_chain_link(ngx_pool_t *pool)
 {
     ngx_chain_t  *cl;
 
     cl = pool->chain;
-
+    /*
+	 * 首先从内存池中去取ngx_chain_t，
+	 * 被清空的ngx_chain_t结构都会放在pool->chain 缓冲链上
+	 */
     if (cl) {
         pool->chain = cl->next;
         return cl;
     }
-
+    /* 如果取不到，则从内存池pool上分配一个数据结构  */
     cl = ngx_palloc(pool, sizeof(ngx_chain_t));
     if (cl == NULL) {
         return NULL;
@@ -72,16 +80,19 @@ ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs)
     ngx_int_t     i;
     ngx_buf_t    *b;
     ngx_chain_t  *chain, *cl, **ll;
-
+    /* 在内存池pool上分配bufs->num个 buf缓冲区 ，每个大小为bufs->size */
     p = ngx_palloc(pool, bufs->num * bufs->size);
     if (p == NULL) {
         return NULL;
     }
 
     ll = &chain;
-
+    /* 循环创建BUF，并且将ngx_buf_t挂载到ngx_chain_t链表上，并且返回链表*/
     for (i = 0; i < bufs->num; i++) {
-
+        /* 最终调用的是内存池pool，开辟一段内存用作缓冲区，主要放置ngx_buf_t结构体
+        Expands to:
+        ngx_pcalloc(pool, sizeof(ngx_buf_t)) 
+        */
         b = ngx_calloc_buf(pool);
         if (b == NULL) {
             return NULL;
@@ -104,16 +115,16 @@ ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs)
         b->temporary = 1;
 
         b->start = p;
-        p += bufs->size;
+        p += bufs->size;//p往前增
         b->end = p;
-
+        /* 分配一个ngx_chain_t */
         cl = ngx_alloc_chain_link(pool);
         if (cl == NULL) {
             return NULL;
         }
-
+        /* 将buf，都挂载到ngx_chain_t链表上，最终返回ngx_chain_t链表 */
         cl->buf = b;
-        *ll = cl;
+        *ll = cl;//第一次运行到这儿时i==0  ll的地址是&chain   后续运行到这儿时  是最后一个链表节点&cl->next的内存位置 
         ll = &cl->next;
     }
 
@@ -122,14 +133,16 @@ ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs)
     return chain;
 }
 
-
+/**
+ * 将其它缓冲区链表放到已有缓冲区链表结构的尾部
+ */
 ngx_int_t
 ngx_chain_add_copy(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)
 {
     ngx_chain_t  *cl, **ll;
 
-    ll = chain;
-
+    ll = chain;//chain 指向指针的指针，很绕
+    /* 找到缓冲区链表结尾部分，cl->next== NULL；cl = *chain既为指针链表地址*/
     for (cl = *chain; cl; cl = cl->next) {
         ll = &cl->next;
     }
@@ -141,10 +154,10 @@ ngx_chain_add_copy(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)
             return NGX_ERROR;
         }
 
-        cl->buf = in->buf;
-        *ll = cl;
-        ll = &cl->next;
-        in = in->next;
+        cl->buf = in->buf;//in上的buf拷贝到cl上面
+        *ll = cl;//并且放到chain链表上最后一个节点的 NEXT节点上
+        ll = &cl->next;//链表往下走  ll保存最新这个节点的next的地址
+        in = in->next;//被拷贝的节点继续遍历，直到NULL
     }
 
     *ll = NULL;
@@ -153,23 +166,28 @@ ngx_chain_add_copy(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)
 }
 
 
+/**
+ * 从空闲的buf链表上，获取一个未使用的buf链表这段代码从名字上来看是获得一个free的buf，
+ * 实际上是从一个free的链表中获得一个chain节点或者是重新分配一个chain节点
+ *nginx中的链表操作很多都是头链表操作，即如果需要添加链表元素的话通常都将该元素添加到头上
+ */
 ngx_chain_t *
 ngx_chain_get_free_buf(ngx_pool_t *p, ngx_chain_t **free)
 {
     ngx_chain_t  *cl;
-
+    /* 空闲列表中有数据，则直接返回 第一个空闲的节点*/
     if (*free) {
-        cl = *free;
-        *free = cl->next;
-        cl->next = NULL;
+        cl = *free;//得到第一个空闲节点
+        *free = cl->next;//空闲链表指向第二个空闲节点
+        cl->next = NULL;//返回前置空next指针，这个指针对使用者没有用
         return cl;
     }
-
+    /*没有找到空闲节点  那么分配一个新的链表节点 */
     cl = ngx_alloc_chain_link(p);
     if (cl == NULL) {
         return NULL;
     }
-
+    /*申请一个buf节点  注意buf节点是哪个比较大 比较复杂的结构*/
     cl->buf = ngx_calloc_buf(p);
     if (cl->buf == NULL) {
         return NULL;
@@ -180,7 +198,12 @@ ngx_chain_get_free_buf(ngx_pool_t *p, ngx_chain_t **free)
     return cl;
 }
 
-
+/*
+需要处理的链表是out指针指向的链表，而free指向的应该就是当前存在的free链表，而busy链表则是当前存在的busy链表，该链表也是待处理的链表
+所以开始的时候需要判断将out应该放到哪一个位置，如果busy当前就存在的话，那么就应该将out放置到busy的最后，如果当前busy链表不存在，那么处理就是
+将其作为busy链表进行处理
+而后面的操作则是说明从头对busy链表实行检查，如果busy链表中的buf还存在需要处理的内存空间，那么就需要停止处理，否则就将其置为空（即对last和pos进行处理）
+*/
 void
 ngx_chain_update_chains(ngx_pool_t *p, ngx_chain_t **free, ngx_chain_t **busy,
     ngx_chain_t **out, ngx_buf_tag_t tag)
--- nginx-1.17.4/src/core/ngx_buf.h
+++ nginx-1.17.4/src/core/ngx_buf.h
@@ -16,41 +16,47 @@
 typedef void *            ngx_buf_tag_t;
 
 typedef struct ngx_buf_s  ngx_buf_t;
-
+/**
+ * Nginx缓冲区
+ * 1. 从上面这个数据结构中，可以看到ngx_buf_t结构，即可以处理内存，也可以处理文件。
+ * 2. Nginx使用了位域的方法，节省存储空间。
+ * 3. 每个buf都记录了开始和结束点以及未处理的开始和结束点，因为缓冲区的内存申请了之后，是可以被复用的。
+ * 4. 所有缓冲区需要的数据结构以及缓冲区的buf内存块都会被分配到内存池上面。
+ */
 struct ngx_buf_s {
-    u_char          *pos;
-    u_char          *last;
-    off_t            file_pos;
-    off_t            file_last;
+    u_char          *pos;/* 待处理数据的开始标记  */
+    u_char          *last;/* 待处理数据的结尾标记 */
+    off_t            file_pos;/* 处理文件时，待处理的文件开始标记  */
+    off_t            file_last;/* 处理文件时，待处理的文件结尾标记  */
 
     u_char          *start;         /* start of buffer */
     u_char          *end;           /* end of buffer */
-    ngx_buf_tag_t    tag;
-    ngx_file_t      *file;
+    ngx_buf_tag_t    tag;/* 缓冲区标记地址，是一个void类型的指针。 */
+    ngx_file_t      *file;/* 引用的文件 */
     ngx_buf_t       *shadow;
 
 
     /* the buf's content could be changed */
-    unsigned         temporary:1;
+    unsigned         temporary:1; /* 标志位，为1时，内存可修改 */
 
     /*
      * the buf's content is in a memory cache or in a read only memory
      * and must not be changed
      */
-    unsigned         memory:1;
+    unsigned         memory:1;/* 标志位，为1时，内存只读 */
 
     /* the buf's content is mmap()ed and must not be changed */
-    unsigned         mmap:1;
+    unsigned         mmap:1;/* 标志位，为1时，mmap映射过来的内存，不可修改 */
 
-    unsigned         recycled:1;
-    unsigned         in_file:1;
-    unsigned         flush:1;
-    unsigned         sync:1;
-    unsigned         last_buf:1;
-    unsigned         last_in_chain:1;
+    unsigned         recycled:1;/* 标志位，为1时，可回收 */
+    unsigned         in_file:1;/* 标志位，为1时，表示处理的是文件 */
+    unsigned         flush:1;/* 标志位，为1时，表示需要进行flush操作 */
+    unsigned         sync:1;/* 标志位，为1时，表示可以进行同步操作，容易引起堵塞 */
+    unsigned         last_buf:1;/* 标志位，为1时，表示为缓冲区链表ngx_chain_t上的最后一块待处理缓冲区 */
+    unsigned         last_in_chain:1;/* 标志位，为1时，表示为缓冲区链表ngx_chain_t上的最后一块缓冲区 */
 
-    unsigned         last_shadow:1;
-    unsigned         temp_file:1;
+    unsigned         last_shadow:1;/* 标志位，为1时，表示是否是最后一个影子缓冲区 */
+    unsigned         temp_file:1;/* 标志位，为1时，表示当前缓冲区是否属于临时文件 */
 
     /* STUB */ int   num;
 };
@@ -148,6 +154,7 @@ ngx_chain_t *ngx_create_chain_of_bufs(ngx_pool_t *pool, ngx_bufs_t *bufs);
 #define ngx_calloc_buf(pool) ngx_pcalloc(pool, sizeof(ngx_buf_t))
 
 ngx_chain_t *ngx_alloc_chain_link(ngx_pool_t *pool);
+//直接交还给Nginx内存池的pool->chain空闲buf链表
 #define ngx_free_chain(pool, cl)                                             \
     cl->next = pool->chain;                                                  \
     pool->chain = cl
